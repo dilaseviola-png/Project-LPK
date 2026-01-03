e3dc11342765d167386819a4ccb80bc5fd64ec37
dilaseviola-png <dilaseviola@gmail.com>
2026-01-03 14:28:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scipy.stats import linregress
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Visualisasi Orde Reaksi", layout="centered")
